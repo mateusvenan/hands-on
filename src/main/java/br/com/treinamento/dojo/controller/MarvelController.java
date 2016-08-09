d41b97d979c56cf2e7b248c1f59d87d060a59c27
Guilherme de Camargo <gui.de.camargo@gmail.com>
2016-08-09 11:38:57 -0300

    MarvelController

--- src/main/java/br/com/treinamento/dojo/controller/MarvelController.java
+++ src/main/java/br/com/treinamento/dojo/controller/MarvelController.java
@@ -2,17 +2,21 @@ package br.com.treinamento.dojo.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+
+import com.google.gson.Gson;
 
 @RestController
 public class MarvelController {
 
+    @CrossOrigin(origins = "http://localhost:8081")
 	@RequestMapping(value = "/series", method = RequestMethod.GET)
-	public ResponseEntity<String> helloWorld() {
-
-		return new ResponseEntity<String>("hello world", HttpStatus.OK);
+	public ResponseEntity<String> helloWorld(@RequestParam(value="startYear", required=false) String startYear) {
+    	Gson gson = new Gson();
+		return new ResponseEntity<String>(gson.toJson("hello world" + startYear), HttpStatus.OK);
 	}
 }
